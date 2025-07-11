28d4d9c6f1e2e7048012898d8aebb1eb5d673266
Stewart McMillan <stewmac570@gmail.com>
2025-07-12 08:53:40 +1000

    feat: Phase 2 multi-log overlay with stable hover line (#2)

--- src/CastleOverlayV2/CastleOverlayV2/MainForm.Designer.cs
+++ src/CastleOverlayV2/CastleOverlayV2/MainForm.Designer.cs
@@ -5,6 +5,10 @@
         private System.ComponentModel.IContainer components = null;
         private ScottPlot.WinForms.FormsPlot formsPlot1;
         private System.Windows.Forms.Button loadCsvButton;
+        private System.Windows.Forms.Button btnLoadRun1;
+        private System.Windows.Forms.Button btnLoadRun2;
+        private System.Windows.Forms.Button btnLoadRun3;
+
 
 
         protected override void Dispose(bool disposing)
@@ -33,13 +37,38 @@
             //
             // loadCsvButton
             //
-            this.loadCsvButton.Location = new System.Drawing.Point(12, 420);
-            this.loadCsvButton.Name = "loadCsvButton";
-            this.loadCsvButton.Size = new System.Drawing.Size(120, 30);
-            this.loadCsvButton.TabIndex = 1;
-            this.loadCsvButton.Text = "Load CSV";
-            this.loadCsvButton.UseVisualStyleBackColor = true;
-            this.loadCsvButton.Click += new System.EventHandler(this.LoadCsvButton_Click);
+            // Inside InitializeComponent()
+
+// Load Run 1
+this.btnLoadRun1 = new System.Windows.Forms.Button();
+this.btnLoadRun1.Location = new System.Drawing.Point(10, 10);
+this.btnLoadRun1.Name = "btnLoadRun1";
+this.btnLoadRun1.Size = new System.Drawing.Size(100, 30);
+this.btnLoadRun1.Text = "Load Run 1";
+this.btnLoadRun1.UseVisualStyleBackColor = true;
+this.btnLoadRun1.Click += new System.EventHandler(this.LoadRun1Button_Click);
+this.Controls.Add(this.btnLoadRun1);
+
+// Load Run 2
+this.btnLoadRun2 = new System.Windows.Forms.Button();
+this.btnLoadRun2.Location = new System.Drawing.Point(10, 50);
+this.btnLoadRun2.Name = "btnLoadRun2";
+this.btnLoadRun2.Size = new System.Drawing.Size(100, 30);
+this.btnLoadRun2.Text = "Load Run 2";
+this.btnLoadRun2.UseVisualStyleBackColor = true;
+this.btnLoadRun2.Click += new System.EventHandler(this.LoadRun2Button_Click);
+this.Controls.Add(this.btnLoadRun2);
+
+// Load Run 3
+this.btnLoadRun3 = new System.Windows.Forms.Button();
+this.btnLoadRun3.Location = new System.Drawing.Point(10, 90);
+this.btnLoadRun3.Name = "btnLoadRun3";
+this.btnLoadRun3.Size = new System.Drawing.Size(100, 30);
+this.btnLoadRun3.Text = "Load Run 3";
+this.btnLoadRun3.UseVisualStyleBackColor = true;
+this.btnLoadRun3.Click += new System.EventHandler(this.LoadRun3Button_Click);
+this.Controls.Add(this.btnLoadRun3);
+
             //
             // MainForm
             //
---
+++ src/CastleOverlayV2/CastleOverlayV2/Utils/ColorMap.cs
@@ -0,0 +1,19 @@
+﻿using ScottPlot;
+
+namespace CastleOverlayV2.Utils
+{
+    public static class ColorMap
+    {
+        public static ScottPlot.Color GetColor(int index)
+        {
+            // Replace with real Castle colors
+            return index switch
+            {
+                0 => Colors.Blue,
+                1 => Colors.Red,
+                2 => Colors.Green,
+                _ => Colors.Black
+            };
+        }
+    }
+}
